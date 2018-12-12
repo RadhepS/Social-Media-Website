48539ceff449d277bd54760ba3fa3a883c954fed
Radhep <Radhep.Saba@gmail.com>
2018-12-11 22:50:07 -0500

    Angular template binding

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent {
+
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+
+  onAddPost(postInput: HTMLTextAreaElement) {
+
+    this.newPost = this.enteredValue;
+  }
+}
