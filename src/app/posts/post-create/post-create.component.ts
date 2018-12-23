250b7c71519c978138ed951347a1e84fcc8c980d
Radhep Saba <Radhep.Saba@gmail.com>
2018-12-23 11:11:56 -0500

    Added observables

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import { Post } from '../post.model';
-import { invalid } from '@angular/compiler/src/render3/view/util';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -13,14 +12,13 @@ export class PostCreateComponent {
 
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter<Post>();
+
+  constructor(public postsService: PostsService) {}
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-
-    const post: Post = {title: form.value.title, content: form.value.content};
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 }
