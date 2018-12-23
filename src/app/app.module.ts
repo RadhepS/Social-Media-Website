4b91057ba196871cd4756baa5f59f4bc97f5bd6f
Radhep Saba <Radhep.Saba@gmail.com>
2018-12-23 10:36:37 -0500

    Added post service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,6 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { PostsService } from './posts/posts.service';
 
 
 @NgModule({
@@ -36,7 +35,7 @@ import { PostsService } from './posts/posts.service';
     MatToolbarModule,
     MatExpansionModule
   ],
-  providers: [PostsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
