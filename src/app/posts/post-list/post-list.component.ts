bb43ebb48a39dab583647ffd5ae781b89cb86c66
Radhep Saba <Radhep.Saba@gmail.com>
2018-12-23 14:36:21 -0500

    Completed Angular front-end

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -9,7 +9,7 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit, OnDestroy{
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   { title: 'First Post', content: 'This is the first post\'s content' },
   //   { title: 'Second Post', content: 'This is the second post\'s content' },
