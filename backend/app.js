56da841b1924bba8bab7162d219468b61f14b929
Radhep <Radhep.Saba@gmail.com>
2018-12-24 12:12:12 -0500

    Updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,52 @@
-const express = require('express');
+const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
 
-const app = express();
+const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-app.use((req, res, next) => {
-  res.send("Hello from express!");
-});
+  return false;
+};
 
-module.exports = app;
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(port);
