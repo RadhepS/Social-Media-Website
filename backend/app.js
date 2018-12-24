b6287cc85db984492bdda4cff7c043cf2be7112c
Radhep Saba <Radhep.Saba@gmail.com>
2018-12-24 18:19:25 -0500

    Connect Front & Back end

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,27 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("First middleware");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
 
-
-app.use((req, res, next) => {
-  res.send("Hello from express!");
+app.use("/api/posts",(req, res, next) => {
+  const posts = [
+    { id: "faaadsd2342",
+     title: "First server-side post",
+     content: "This is coming from the server"
+    },
+    { id: "kasddfs",
+    title: "Second server-side post",
+    content: "This is coming from the server!"
+    }
+  ];
+  res.status(200).json({
+    message: "Posts feteched successfully",
+    posts: posts
+  });
 });
 
 module.exports = app;
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -21,8 +21,8 @@ export class PostListComponent implements OnInit, OnDestroy {
   constructor( public postsService: PostsService) {}
 
   ngOnInit() {
-    this.posts = this.postsService.getPosts();
-  this.postsSub = this.postsService.getPostUpdatedListener()
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdatedListener()
       .subscribe((posts: Post[]) => {
         this.posts = posts;
       });
