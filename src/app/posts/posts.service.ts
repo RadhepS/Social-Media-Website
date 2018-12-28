ea9341953ec552a1cb9998e55c69c7b6dd88f0c9
Radhep Saba <Radhep.Saba@gmail.com>
2018-12-28 15:18:58 -0500

    small fix

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -21,7 +21,7 @@ export class PostsService {
           return {
             title: post.title,
             content: post.content,
-            id: post._id
+            id: post._id,
             imagePath: post.imagePath
           };
         });
