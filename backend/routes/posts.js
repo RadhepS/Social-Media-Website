75c68d1bd06bffd40405581ac0f24b46f2c80b3b
Radhep Saba <Radhep.Saba@gmail.com>
2018-12-29 15:39:13 -0500

    Added pagination logic to the server

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -67,9 +67,17 @@ router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) 
 });
 
 router.get("",(req, res, next) => {
-  Post.find()
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  if (pageSize && currentPage) {
+    postQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  postQuery.find()
     .then((documents) => {
-      console.log(documents);
+      // console.log(documents);
       res.status(200).json({
         message: "Posts feteched successfully",
         posts: documents
