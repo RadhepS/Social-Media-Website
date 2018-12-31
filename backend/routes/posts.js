79cc13715f76679c3466e41a359ba6c67fe411e2
Radhep Saba <Radhep.Saba@gmail.com>
2018-12-30 22:37:06 -0500

    Token authorization added

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const multer = require("multer");
 
+const Post = require('../models/post');
+const checkAuth = require('../middleware/check-auth')
+
 const router = express.Router();
 
 const MIME_TYPE_MAP = {
@@ -9,7 +12,6 @@ const MIME_TYPE_MAP = {
   'image/jpg': 'jpg'
 }
 
-const Post = require('../models/post');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -28,7 +30,7 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
+router.post("", checkAuth, multer({storage: storage}).single("image"), (req, res, next) => {
   const url = req.protocol + '://' + req.get("host");
   const post = new Post({
     title: req.body.title,
@@ -46,7 +48,7 @@ router.post("", multer({storage: storage}).single("image"), (req, res, next) => 
   });
 });
 
-router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
+router.put("/:id", checkAuth, multer({storage: storage}).single("image"), (req, res, next) => {
   let imagePath = req.body.imagePath;
   if (req.file) {
   const url = req.protocol + '://' + req.get("host");
@@ -98,7 +100,7 @@ router.get("/:id", (req, res, next) => {
   })
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then((result) => {
     console.log(result);
     res.status(200).json({message: 'Post deleted!'});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {
   MatInputModule,
   MatCardModule,
@@ -20,6 +20,7 @@ import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 
 @NgModule({
@@ -46,7 +47,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     MatPaginatorModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
