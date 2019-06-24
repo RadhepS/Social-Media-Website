b3f69eec030095b0f142088ebb47f72c7cf5afcb
Radhep <Radhep.Saba@gmail.com>
2019-06-23 20:07:32 -0400

    Refactored code, added proper TSLint and Prettier settings

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,36 +1,41 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Subscription } from 'rxjs';
-import { SignupData } from './signup-data.model';
-
+import { ISignupData } from './signup-data.model';
 
 @Component({
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit, OnDestroy {
-  isLoading = false;
+  public isLoading = false;
   private authStatusSub: Subscription;
 
   constructor(public authService: AuthService) {}
 
-  ngOnInit() {
-    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(() => {
-      this.isLoading = false;
-    });
+  public ngOnInit() {
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(() => {
+        this.isLoading = false;
+      });
   }
 
-  onSignup(form: NgForm) {
+  public onSignup(form: NgForm) {
     if (form.invalid) {
       return;
     }
     this.isLoading = true;
-    const signupData: SignupData = {username: form.value.username, email: form.value.email, password: form.value.password};
+    const signupData: ISignupData = {
+      username: form.value.username,
+      email: form.value.email,
+      password: form.value.password
+    };
     this.authService.createUser(signupData);
   }
 
-  ngOnDestroy() {
+  public ngOnDestroy() {
     this.authStatusSub.unsubscribe();
   }
 }
